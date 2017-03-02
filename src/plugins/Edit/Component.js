f05b416e9e8c03222f9d1415a4184b8563d636be
Greg Chamberlain <gregchamberlain94@gmail.com>
2017-03-02 13:38:44 -0800

    Updated Edit plugin component to have random background color

--- src/plugins/Edit/Component.js
+++ src/plugins/Edit/Component.js
@@ -13,10 +13,17 @@ const mapDispatchToProps = dispatch => ({
   insertItem: () => dispatch(insertOrMoveItem('root', 0, {
     type: 'div',
     props: {
-      style: { height: 50, background: 'green', margin: 5 }
+      style: { height: 50, background: getBG(), margin: 5 },
     },
     children: []
   }))
 });
+
+const getBG = () => {
+  let r = (Math.floor(Math.random()*255)).toString();
+  let g = (Math.floor(Math.random()*255)).toString();
+  let b = (Math.floor(Math.random()*255)).toString();
+  return `rgb(${r}, ${g}, ${b})`;
+}
 
 export default connect(null, mapDispatchToProps)(TestComp);
