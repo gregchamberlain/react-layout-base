4263d40e855c8837ec1b89d3d8cba4ede029c88c
Greg Chamberlain <gregchamberlain94@gmail.com>
2017-03-03 18:24:43 -0800

    Silly additions to DnD plugin

--- docs/src/plugins/DnD/Component.js
+++ docs/src/plugins/DnD/Component.js
@@ -12,16 +12,32 @@ const target = {
   }
 };
 
-const TrashBin = ({ connectDropTarget, isOver, canDrop }) => connectDropTarget(
-  <div style={{
-    width: 100,
-    height: 100,
-    backgroundColor: canDrop ? ( isOver ? '#f44336' : '#ffcdd2' ) : '#64B5F6',
-    border: '1px solid #ccc'
-  }}>
-    Trash Bin
+const TrashBin = ({ connectDropTarget, ...props }) => connectDropTarget(
+  <div style={style(props)}>
+    <svg xmlns="http://www.w3.org/2000/svg" fill="inherit" height="24" viewBox="0 0 24 24" width="24">
+      <path d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zM19 4h-3.5l-1-1h-5l-1 1H5v2h14V4z"/>
+      <path d="M0 0h24v24H0z" fill="none"/>
+    </svg>
   </div>
 );
+
+const style = ({ canDrop, isOver }) => ({
+  position: 'absolute',
+  backgroundColor: isOver ? 'red' : 'grey',
+  width: canDrop ? 75 : 0,
+  height: canDrop ? 75 : 0,
+  opacity: canDrop ? 1 : 0,
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  fontSize: 28,
+  color: 'white',
+  transform: 'translateX(50%) translateY(50%)',
+  bottom: 75,
+  right: 75,
+  borderRadius: '50%',
+  transition: 'opacity 0.5s ease-in, backgroundColor 0.2s linear'
+});
 
 const droppable = DropTarget('Component', target, (conn, monitor) => ({
   connectDropTarget: conn.dropTarget(),
